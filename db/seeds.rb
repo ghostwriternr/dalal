29c0440610fea3795cce1ac862e0345d3db36f2f
Naresh R <ghostwriternr@gmail.com>
2020-09-12 20:47:40 +0530

    feat: Seed templates into DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Template.destroy_all
+Dir.foreach('functions/template') do |folderName|
+  next if (folderName == '.') || (folderName == '..')
+
+  functionTemplateFolder = "functions/template/#{folderName}/function"
+  case folderName
+  when 'python3'
+    templateContent = File.read("#{functionTemplateFolder}/handler.py")
+    Template.create(content: templateContent, language: 'python')
+  when 'node'
+    templateContent = File.read("#{functionTemplateFolder}/handler.js")
+    Template.create(content: templateContent, language: 'javascript')
+  end
+end
